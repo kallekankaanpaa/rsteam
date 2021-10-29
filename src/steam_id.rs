2c5748d444c24c491a8a1fc79482b1a8e35209ed
Kalle Kankaanpää <unset>
2021-10-29 20:33:39 +0300

    Renamed ID's and added conversions between SteamID and SteamID2

--- src/steam_id.rs
+++ src/steam_id.rs
@@ -1,73 +1,158 @@
-use lazy_static::lazy_static;
-use regex::Regex;
+use std::convert::{TryFrom, TryInto};
+use std::fmt;
 use std::num::ParseIntError;
 use std::str::FromStr;
 
-const v: u64 = 0x0110000100000000_u64;
+const INDIVIDUAL_IDENTIFIER: u64 = 0x0110000100000000_u64;
+const GROUP_IDENTIFIER: u64 = 0x0170000000000000_u64;
 
-#[derive(PartialEq, Debug)]
-pub struct SteamID(u64);
+#[derive(Debug)]
+pub enum SteamIDError {
+    ParseError,
+    ConversionError,
+}
 
-impl FromStr for SteamID {
-    type Err = ParseIntError;
+impl From<ParseIntError> for SteamIDError {
+    fn from(_: ParseIntError) -> Self {
+        SteamIDError::ParseError
+    }
+}
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        lazy_static! {
-            static ref ID_RE: Regex = Regex::new(r"^STEAM_\d:(\d):(\d{8})$").unwrap();
-            static ref ID32_RE: Regex = Regex::new(r"\[[IUMGAPCgTLca]:\d:(\d{9})\]$").unwrap();
-        }
+#[repr(u8)]
+#[derive(PartialEq, Debug, Clone, Copy)]
+pub enum Universe {
+    Individual = 0,
+    Public = 1,
+    Beta = 2,
+    Internal = 3,
+    Dev = 4,
+    RC = 5,
+}
 
-        if let Some(captures) = ID_RE.captures(s) {
-            let y = captures.get(1).unwrap().as_str().parse::<u64>().unwrap();
-            let z = captures.get(2).unwrap().as_str().parse::<u64>().unwrap();
-            Ok(SteamID(z * 2 + v + y))
-        } else if let Some(captures) = ID32_RE.captures(s) {
-            let account_id = captures.get(1).unwrap().as_str().parse::<u64>().unwrap();
-            let y = account_id & 1;
-            let z = account_id / 2_u64;
-            Ok(SteamID(z * 2 + v + y))
-        } else {
-            s.parse::<u64>().map(|id| SteamID(id))
+impl TryFrom<u8> for Universe {
+    type Error = SteamIDError;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(Universe::Individual),
+            1 => Ok(Universe::Public),
+            2 => Ok(Universe::Beta),
+            3 => Ok(Universe::Internal),
+            4 => Ok(Universe::Dev),
+            5 => Ok(Universe::RC),
+            _ => Err(SteamIDError::ParseError),
         }
     }
 }
 
-impl SteamID {
-    pub fn new(id: u64) -> Self {
-        SteamID(id)
+impl FromStr for Universe {
+    type Err = SteamIDError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        s.parse::<u8>()?.try_into()
     }
+}
 
-    pub fn id64(&self) -> u64 {
-        self.0
-    }
+/* SteamID can only be used to represent individual(user) steamids */
+#[derive(PartialEq, Debug)]
+pub struct SteamID2 {
+    pub universe: Universe,
+    pub id_number: u8,
+    pub account_number: u32,
+}
 
-    pub fn id32(&self) -> String {
-        let account_id = self.0 - v;
-        let y = self.0 & 1;
-        format!("[{}:{}:{}]", "U", y, account_id)
-    }
+impl FromStr for SteamID2 {
+    type Err = SteamIDError;
 
-    pub fn raw(&self) -> String {
-        let y = self.0 & 1;
-        let z = (self.0 - v - y) / 2;
-        let x = 1_u64; // where from?
-        format!("STEAM_{}:{}:{}", x, y, z)
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let universe = s[6..7].parse::<Universe>()?;
+        let id_number = s[8..9].parse::<u8>()?;
+        let account_number = s[10..].parse::<u32>()?;
+
+        Ok(SteamID2 {
+            universe,
+            id_number,
+            account_number,
+        })
     }
+}
+
+impl fmt::Display for SteamID2 {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "STEAM_{}:{}:{}",
+            self.universe as u8, self.id_number, self.account_number
+        )
+    }
+}
+
+impl TryFrom<SteamID> for SteamID2 {
+    type Error = SteamIDError;
+    fn try_from(id: SteamID) -> Result<Self, Self::Error> {
+        let id_type = (id.0 << 8) >> 52;
+        if id_type == 1 {
+            let x = (id.0 >> 56) as u8;
+            let y = id.0 & 1;
+            let z = (id.0 - y - INDIVIDUAL_IDENTIFIER) / 2;
+            Ok(SteamID2 {
+                universe: x.try_into()?,
+                id_number: y as u8,
+                account_number: z as u32,
+            })
+        } else {
+            Err(SteamIDError::ConversionError)
+        }
+    }
+}
+
+pub struct SteamID(u64);
+
+impl From<SteamID2> for SteamID {
+    fn from(id: SteamID2) -> Self {
+        SteamID(id.account_number as u64 * 2 + INDIVIDUAL_IDENTIFIER + id.id_number as u64)
+    }
+}
+
+pub enum IDType {
+    Invalid,
+    Individual,
+    Multiseat,
+    GameServer,
+    AnonGameServer,
+    Pending,
+    ContentServer,
+    Clan,
+    Chat,
+    AnonUser,
+}
+
+pub struct SteamID3 {
+    pub id_type: IDType,
+    pub universe: Universe,
+    pub account_id: u32,
 }
 
 mod tests {
     use super::*;
 
     #[test]
-    fn parse_steam_id() {
-        assert_eq!(
-            "STEAM_0:0:50503027".parse::<SteamID>().unwrap(),
-            SteamID(76561198061271782)
-        );
+    fn steam_id() {
+        let steam_id = SteamID2 {
+            universe: Universe::Individual,
+            id_number: 0,
+            account_number: 50503027,
+        };
+        assert_eq!("STEAM_0:0:50503027".parse::<SteamID2>().unwrap(), steam_id);
 
+        assert_eq!(format!("{}", steam_id), "STEAM_0:0:50503027");
+
+        /*
         assert_eq!(
             "[U:1:101006054]".parse::<SteamID>().unwrap(),
             SteamID(76561198061271782)
         );
+        group id = 103582791469184191
+        user id =  76561198061271782
+        */
     }
 }
