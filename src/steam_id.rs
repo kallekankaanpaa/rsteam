3b30344f794a2cbfa9e06b912fd47d5b5e92b24b
Kalle Kankaanpää <unset>
2021-10-29 22:00:24 +0300

    Added SteamID3 implementations

--- src/steam_id.rs
+++ src/steam_id.rs
@@ -123,13 +123,115 @@ pub enum IDType {
     ContentServer,
     Clan,
     Chat,
+    P2PSuperSeeder,
     AnonUser,
+}
+
+impl FromStr for IDType {
+    type Err = SteamIDError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "I" => Ok(IDType::Invalid),
+            "U" => Ok(IDType::Individual),
+            "M" => Ok(IDType::Multiseat),
+            "G" => Ok(IDType::GameServer),
+            "A" => Ok(IDType::AnonGameServer),
+            "P" => Ok(IDType::Pending),
+            "C" => Ok(IDType::ContentServer),
+            "g" => Ok(IDType::Clan),
+            "T" | "L" | "c" => Ok(IDType::Chat),
+            "a" => Ok(IDType::AnonUser),
+            _ => Err(SteamIDError::ParseError),
+        }
+    }
+}
+
+impl TryFrom<u8> for IDType {
+    type Error = SteamIDError;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(IDType::Invalid),
+            1 => Ok(IDType::Individual),
+            2 => Ok(IDType::Multiseat),
+            3 => Ok(IDType::GameServer),
+            4 => Ok(IDType::AnonGameServer),
+            5 => Ok(IDType::Pending),
+            6 => Ok(IDType::ContentServer),
+            7 => Ok(IDType::Clan),
+            8 => Ok(IDType::Chat),
+            9 => Ok(IDType::P2PSuperSeeder),
+            10 => Ok(IDType::AnonUser),
+            _ => Err(SteamIDError::ParseError),
+        }
+    }
+}
+
+impl fmt::Display for IDType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let letter = match self {
+            IDType::Invalid => 'I',
+            IDType::Individual => 'U',
+            IDType::Multiseat => 'M',
+            IDType::GameServer => 'G',
+            IDType::AnonGameServer => 'A',
+            IDType::Pending => 'P',
+            IDType::ContentServer => 'C',
+            IDType::Clan => 'g',
+            IDType::Chat => 'T', // How about 'l' or 'c'
+            IDType::AnonUser => 'a',
+            IDType::P2PSuperSeeder => panic!("P2PSuperSeeder doesn't have letter-representation"),
+        };
+        write!(f, "{}", letter)
+    }
 }
 
 pub struct SteamID3 {
     pub id_type: IDType,
     pub universe: Universe,
-    pub account_id: u32,
+    pub account_number: u32,
+}
+
+impl FromStr for SteamID3 {
+    type Err = SteamIDError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let id_type = s[1..2].parse::<IDType>()?;
+        let universe = s[3..4].parse::<Universe>()?;
+        let account_number = s[5..s.len() - 1].parse::<u32>()?;
+
+        Ok(SteamID3 {
+            id_type,
+            universe,
+            account_number,
+        })
+    }
+}
+
+impl fmt::Display for SteamID3 {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "[{}:{}:{}]",
+            self.id_type, self.universe as u8, self.account_number
+        )
+    }
+}
+
+impl TryFrom<SteamID> for SteamID3 {
+    type Error = SteamIDError;
+
+    fn try_from(id: SteamID) -> Result<Self, Self::Error> {
+        let universe = ((id.0 >> 56) as u8).try_into()?;
+        let id_type = (((id.0 << 8) >> 52) as u8).try_into()?;
+        let account_number = (id.0 >> 32) as u32;
+        Ok(SteamID3 {
+            id_type,
+            universe,
+            account_number,
+        })
+    }
 }
 
 mod tests {
