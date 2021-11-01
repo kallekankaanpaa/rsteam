55b57f86e5edb4d0de756300eb7fe781ccfcccdb
Kalle Kankaanpää <kalle.kankaanpaa@protonmail.com>
2021-11-01 21:45:24 +0200

    Added first example

---
+++ examples/README.md
@@ -0,0 +1,14 @@
+# Examples of using rsteam
+
+## Dependencies
+
+These examples use these dependencies:
+```
+[dependencies]
+rsteam = "0.0.1"
+tokio = { version = "1", features = ["full"] }
+``` 
+
+## Other requirements
+
+These examples require you to have your steam api key in an environment variable named `STEAM_API_KEY`.
---
+++ examples/get_steamid.rs
@@ -0,0 +1,30 @@
+use std::env;
+
+use rsteam::SteamClient;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let api_key = match env::var("STEAM_API_KEY") {
+        Ok(key) => key,
+        Err(_) => {
+            println!("Remember to set the STEAM_API_KEY environment variable");
+            return Ok(());
+        }
+    };
+
+    let vanity_url = match env::args().nth(1) {
+        Some(url) => url,
+        None => {
+            println!("Please provide vanity url (custom steam id).");
+            return Ok(());
+        }
+    };
+
+    let client = SteamClient::new(&api_key);
+
+    let id = client.resolve_vanity_url(&vanity_url).await?;
+
+    println!("{}", id);
+
+    Ok(())
+}
