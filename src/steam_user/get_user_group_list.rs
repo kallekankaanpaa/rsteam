9de49fd0b7af622fb76c15c797c5ba1799a6cc6c
Kalle Kankaanpää <kalle.kankaanpaa@protonmail.com>
2021-11-05 00:19:37 +0200

    Implemented get_user_group_list function for SteamClient

---
+++ src/steam_user/get_user_group_list.rs
@@ -0,0 +1,68 @@
+use crate::utils::{Error, ResponseWrapper, Result, AUTHORITY};
+use crate::{SteamClient, SteamID};
+
+use hyper::body::to_bytes;
+use hyper::Uri;
+
+use serde::Deserialize;
+use serde_json::from_slice;
+
+const PATH: &str = "/ISteamUser/GetUserGroupList/v1";
+
+#[derive(Deserialize)]
+struct Group {
+    gid: String,
+}
+
+#[derive(Deserialize)]
+struct Resp {
+    success: bool,
+    groups: Vec<Group>,
+}
+
+type Response = ResponseWrapper<Resp>;
+
+impl SteamClient {
+    /// Fetches vector of [SteamIDs](SteamID) which represent the ids for the users groups
+    pub async fn get_user_group_list(&self, id: SteamID) -> Result<Vec<SteamID>> {
+        let query = format!("key={}&steamid={}", self.api_key, id);
+        let uri = Uri::builder()
+            .scheme("https")
+            .authority(AUTHORITY)
+            .path_and_query(format!("{}?{}", PATH, query))
+            .build()?;
+
+        let response = self.client.get(uri).await?;
+        let body = response.into_body();
+        let parsed = from_slice::<Response>(&to_bytes(body).await?)?;
+
+        let Resp { success, groups } = parsed.response;
+
+        if success {
+            Ok(groups
+                .into_iter()
+                .filter_map(|g| g.gid.parse::<u64>().ok())
+                .map(|n| n.into())
+                .collect::<Vec<SteamID>>())
+        } else {
+            Err(Error {
+                cause: "Request failed".to_owned(),
+            })
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::env;
+
+    #[test]
+    fn fetches_list_of_ids() {
+        let client = SteamClient::new(&env::var("STEAM_API_KEY").unwrap());
+        let groups =
+            tokio_test::block_on(client.get_user_group_list(SteamID::from(76561198061271782)))
+                .unwrap();
+        assert!(!groups.is_empty());
+    }
+}
--- src/steam_user/mod.rs
+++ src/steam_user/mod.rs
@@ -3,6 +3,7 @@
 mod get_friend_list;
 mod get_player_bans;
 mod get_player_summaries;
+mod get_user_group_list;
 mod resolve_vanity_url;
 
 pub use get_friend_list::{Friend, Relation};
