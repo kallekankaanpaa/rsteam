aacf97c539be8ea3f4ba01b04c464a5416bc96ff
Kalle Kankaanpää <kalle.kankaanpaa@protonmail.com>
2021-11-05 19:37:06 +0200

    Improved client documentation

--- src/client.rs
+++ src/client.rs
@@ -4,13 +4,19 @@ use hyper::Client as HyperClient;
 
 use hyper_tls::HttpsConnector;
 
-/// A client which https to access the Steam API
+/// Client to make API requests easily.
+///
+/// Client can be constructed with or without an API key. Only subset
+/// of APIs are available for the client.
 pub struct SteamClient {
     pub(crate) client: HyperClient<HttpsConnector<HttpConnector>, Body>,
     pub(crate) api_key: Option<String>,
 }
 
 impl SteamClient {
+    /// Create a client with an API key.
+    ///
+    /// Client with API key can use all available APIs.
     pub fn with_api_key(key: &str) -> Self {
         let https_connector = HttpsConnector::new();
 
@@ -20,6 +26,9 @@ impl SteamClient {
         }
     }
 
+    /// Create a client without an API key.
+    ///
+    /// Client without an API key can only use a subset of the APIs.
     pub fn new() -> Self {
         let https_connector = HttpsConnector::new();
 
