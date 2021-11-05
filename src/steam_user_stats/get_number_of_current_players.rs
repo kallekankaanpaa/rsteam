272b281c1ecbfd81cb9982886ea878c2c704ef97
Kalle Kankaanpää <kalle.kankaanpaa@protonmail.com>
2021-11-05 21:19:03 +0200

    Added implementation for get_number_of_current_players

---
+++ src/steam_user_stats/get_number_of_current_players.rs
@@ -0,0 +1,75 @@
+use std::num::NonZeroU32;
+
+use crate::client::SteamClient;
+use crate::utils::{Error, ResponseWrapper, Result, AUTHORITY};
+use hyper::body::to_bytes;
+use hyper::Uri;
+use serde::Deserialize;
+
+const PATH: &str = "/ISteamUserStats/GetNumberOfCurrentPlayers/v1/";
+
+#[derive(Deserialize)]
+struct PlayerCount {
+    player_count: Option<u32>,
+    result: u32,
+}
+
+type Response = ResponseWrapper<PlayerCount>;
+
+impl SteamClient {
+    /// Returns the player count of the app/game
+    ///
+    /// Works without an API key.
+    pub async fn get_number_of_current_players(&self, game_id: NonZeroU32) -> Result<u32> {
+        let query = format!("appid={}", game_id);
+        let uri = Uri::builder()
+            .scheme("https")
+            .authority(AUTHORITY)
+            .path_and_query(format!("{}?{}", PATH, query))
+            .build()?;
+
+        let raw_response = self.client.get(uri).await?;
+        let raw_body = raw_response.into_body();
+        let response: Response =
+            serde_json::from_slice(&to_bytes(raw_body).await?).map_err(|_| Error {
+                cause: "No game with game_id".to_owned(),
+            })?;
+
+        let PlayerCount {
+            player_count,
+            result,
+        } = response.response;
+
+        if result == 1 && player_count.is_some() {
+            Ok(player_count.unwrap())
+        } else {
+            Err(Error {
+                cause: "Request failed check that game_id is valid".to_owned(),
+            })
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tokio_test::{assert_err, block_on};
+
+    #[test]
+    fn correct_csgo_achievements() {
+        let client = SteamClient::new();
+        let game_id = NonZeroU32::new(730).unwrap();
+        let player_count = block_on(client.get_number_of_current_players(game_id)).unwrap();
+
+        assert!(player_count > 100000);
+    }
+
+    #[test]
+    fn unknown_game_id_handeled_correctly() {
+        let client = SteamClient::new();
+        let game_id = NonZeroU32::new(731).unwrap();
+        let result = block_on(client.get_number_of_current_players(game_id));
+
+        assert_err!(result);
+    }
+}
--- src/steam_user_stats/mod.rs
+++ src/steam_user_stats/mod.rs
@@ -1,5 +1,6 @@
 //! Implementations for the ISteamUserStats interface
 
 mod get_global_achievement_percentages_for_app;
+mod get_number_of_current_players;
 
 pub use get_global_achievement_percentages_for_app::AchievementData;
